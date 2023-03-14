8537463841e848e45ff1cae21cfc2c4a521cb275
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-14 12:58:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  if (someValue <= 400) {
+    return ("This one is on me!")
+  } else if (someValue > 2000 && someValue <= 2500){
+    return ("I will gladly take your thirty bucks.")
+  } else if (someValue > 2500) {
+    return ("No can do.")
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(isCity){
   // Write your code here!
+  return ( isCity === 'NYC' ? 'Ok, sounds good.' : 'No go.' )
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return 'Bye.';
+  }
 }
